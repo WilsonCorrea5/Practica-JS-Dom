faa7296e8419d978e382138c88490c1db22763c8
WilsonCorrea <wilzon01@gmail.com>
2023-01-10 00:53:51 -0500

    multiples cambios #2. fusion del main nav con mobilenav, separamos el css embebido hacia el styles.css y agregamos funciona toggle al main.js.

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuMobileBurg = document.querySelector('.mobileBurgerMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleMenuRight)
+menuMobileBurg.addEventListener('click', toggleMenuLeft)
 
 function toggleMenuRight() {
     desktopMenu.classList.toggle('inactive');
+    
+}
+
+function toggleMenuLeft() {
+    mobileMenu.classList.toggle('inactive');
     
 }
