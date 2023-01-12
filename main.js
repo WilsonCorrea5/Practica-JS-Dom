b645efc8e2b0ef18d43afaf7afb99ac26412c85b
WilsonCorrea <wilzon01@gmail.com>
2023-01-12 00:54:51 -0500

    multiples cambios #3. integracion del menu carrito, su css y funcion en  js, se renombraron y modificaron funciones

--- main.js
+++ main.js
@@ -4,16 +4,27 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobileBurg = document.querySelector('.mobileBurgerMenu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const asideShoppingCart = document.querySelector('.product-detail')
 
-menuEmail.addEventListener('click', toggleMenuRight)
-menuMobileBurg.addEventListener('click', toggleMenuLeft)
+menuEmail.addEventListener('click', showMenuUser);
+menuMobileBurg.addEventListener('click', showMenuMobile);
+menuShoppingCart.addEventListener('click', showMenuMyOrder);
 
-function toggleMenuRight() {
+function showMenuUser() {
     desktopMenu.classList.toggle('inactive');
-    
+    mobileMenu.classList.add('inactive');
+    asideShoppingCart.classList.add('inactive');
 }
 
-function toggleMenuLeft() {
+function showMenuMobile() {
     mobileMenu.classList.toggle('inactive');
-    
+    desktopMenu.classList.add('inactive');
+    asideShoppingCart.classList.add('inactive');
 }
+
+function showMenuMyOrder() {
+    asideShoppingCart.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+}   
